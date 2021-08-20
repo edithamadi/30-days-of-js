1af7cab5fd5456310e69c891229edf1c578fb1fe
edith amadi <edithmcamadi5@gmail.com>
2021-08-20 19:57:26 +0300

    added task1

---
+++ tasks/task1/index.js
@@ -0,0 +1,9 @@
+const person={
+    firstName:"Edith",
+    surname: "Amadi",
+    age:30,
+    sex:"Female",
+    maritalStatus:"single",
+    nationality:"Kenyan",
+}
+console.log("my name is "+ person.firstName,person.surname, "i am "+ person.age, "i am a "+ person.sex, "i am also a "+ person.nationality, "i am "+ person.maritalStatus)
